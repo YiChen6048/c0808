0eec9db6c58aedf026c7013a461f451848869dcd
Yi Chen Tan <yi-ctan@u80z04s03.42kl.edu.my>
2025-03-20 14:59:26 +0800

    added ex00

---
+++ ex00/ft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yi-ctan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 21:06:08 by yi-ctan           #+#    #+#             */
+/*   Updated: 2025/03/19 21:12:28 by yi-ctan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+#define FT_H
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
+
+#endif
+
+
